4f370f7d9e419b4b18b480fc09ad83397e1e9fd5
V. Timchenko (home) <vitaliy.timchenko@privatbank.ua>
2023-04-15 16:21:37 +0300

    rename to allow adding another simple classes (solutions)

--- src/test/java/org/example/WordsCounterTest.java
+++ src/test/java/org/example/WordsCounterTest.java
@@ -6,16 +6,16 @@ import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
-public class SolutionTest {
+public class WordsCounterTest {
 
     @Test
     public void testTopKFrequent() {
         assertEquals(
                 Arrays.asList("a", "i"),
-                Arrays.asList(Solution.topKFrequent_v3(new String[]{"a", "a", "i", "love", "ing", "i", "love", "coding"}, 2)));
+                Arrays.asList(WordsCounter.topKFrequent_v3(new String[]{"a", "a", "i", "love", "ing", "i", "love", "coding"}, 2)));
 
         assertEquals(
                 Arrays.asList("the", "is", "sunny", "day"),
-                Arrays.asList(Solution.topKFrequent_v3(new String[]{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 4)));
+                Arrays.asList(WordsCounter.topKFrequent_v3(new String[]{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 4)));
     }
 }
